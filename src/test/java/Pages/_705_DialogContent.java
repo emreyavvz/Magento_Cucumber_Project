6aacc3c007ca971bac2dfc95ebecfaf520180202
Emre Yavuz <emreeyavuzz98@gmail.com>
2024-04-17 22:40:02 +0300

    705 update

--- src/test/java/Pages/_705_DialogContent.java
+++ src/test/java/Pages/_705_DialogContent.java
@@ -2,6 +2,7 @@ package Pages;
 
 import Utilities.GWD;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 
 public class _705_DialogContent extends ParentPage {
@@ -10,10 +11,58 @@ public class _705_DialogContent extends ParentPage {
         PageFactory.initElements(GWD.getDriver(), this);
     }
 
+    @FindBy(xpath = "//*[@id=\"email\"]")
+    public WebElement Email;
+    @FindBy(xpath = "//*[@id=\"pass\"]")
+    public WebElement Password;
+    @FindBy(xpath = "//*[@id=\"send2\"]")
+    public WebElement SignIn;
+
+    @FindBy(xpath = "//a[@aria-label='store logo']")
+    public WebElement StoreLogo;
+
+    @FindBy(xpath = "//span[@class='action more button']")
+    public WebElement ShopNewYoga;
+
+    @FindBy(xpath = "//li[1]//div[1]//div[1]//div[4]//div[1]//div[1]//form[1]//button[1]//span[1]")
+    public WebElement ShortItem;
+
+    @FindBy(xpath = "//div[@data-bind='html: $parent.prepareMessageForHtml(message.text)']")
+    public WebElement ItemAddedMessage;
+
+    @FindBy(xpath = "//div[@class='swatch-opt-1997']//div[@id='option-label-size-143-item-171']")
+    public WebElement ItemSize;
+
+    @FindBy(xpath = "//div[@class='swatch-opt-1997']//div[@id='option-label-color-93-item-49']")
+    public WebElement ItemColor;
+
+    @FindBy(xpath = "//li[1]//div[1]//div[1]//div[4]//div[1]//div[1]//form[1]//button[1]//span[1]")
+    public WebElement AddToCart;
+
+
     public WebElement getWebElement(String strElement) {
 
         switch (strElement) {
-
+            case "Email":
+                return this.Email;
+            case "Password":
+                return this.Password;
+            case "SignIn":
+                return this.SignIn;
+            case "StoreLogo":
+                return this.StoreLogo;
+            case "ShopNewYoga":
+                return this.ShopNewYoga;
+            case "ShortItem":
+                return this.ShortItem;
+            case "ItemAddedMessage":
+                return this.ItemAddedMessage;
+            case "ItemSize":
+                return this.ItemSize;
+            case "ItemColor":
+                return this.ItemColor;
+            case "AddToCart":
+                return this.AddToCart;
         }
 
         return null;
--- src/test/java/StepDefinitions/_705_StepDefinitions.java
+++ src/test/java/StepDefinitions/_705_StepDefinitions.java
@@ -1,4 +1,64 @@
 package StepDefinitions;
 
+import Pages._703_DialogContent;
+import Pages._705_DialogContent;
+import Utilities.GWD;
+import io.cucumber.java.en.And;
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
 public class _705_StepDefinitions {
+
+    _705_DialogContent dc5 = new _705_DialogContent();
+
+    @Given("The user is logged into their account")
+    public void TheUserIsLoggedIntoTheirAccount() {
+        GWD.getDriver().get("https://magento.softwaretestingboard.com/customer/account/login/");
+        dc5.mySendKeys(dc5.Email, "a2032@proton.me");
+        dc5.mySendKeys(dc5.Password, "L8eAk57*v_KqsjZ");
+        dc5.myClick(dc5.SignIn);
+    }
+
+    @When("The user browses the homepage")
+    public void theUserBrowsesTheHomepage() {
+        dc5.myClick(dc5.StoreLogo);
+    }
+
+    @And("The user adds a randomly selected product to the cart")
+    public void theUserAddsARandomlySelectedProductToTheCart() {
+        dc5.myClick(dc5.ItemSize);
+        dc5.myClick(dc5.ItemColor);
+        dc5.myClick(dc5.AddToCart);
+    }
+
+    @Then("The product should be added successfully")
+    public void theProductShouldBeAddedSuccessfully() {
+        dc5.verifyContainsText(dc5.ItemAddedMessage, "You added Echo Fit Compression Short to your");
+    }
+
+    @And("The user selects the quantity of the product")
+    public void theUserSelectsTheQuantityOfTheProduct() {
+    }
+
+    @Then("The quantity of the product in the cart should be updated accordingly")
+    public void theQuantityOfTheProductInTheCartShouldBeUpdatedAccordingly() {
+    }
+
+    @And("the user has products in their cart")
+    public void theUserHasProductsInTheirCart() {
+    }
+
+    @When("the user removes a product from the cart")
+    public void theUserRemovesAProductFromTheCart() {
+    }
+
+    @Then("the product should be successfully removed")
+    public void theProductShouldBeSuccessfullyRemoved() {
+    }
+
+    @And("The user clicks on shop")
+    public void theUserClicksOnShop() {
+        dc5.myClick(dc5.ShopNewYoga);
+    }
 }
